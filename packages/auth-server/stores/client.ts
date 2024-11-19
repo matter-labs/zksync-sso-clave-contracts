a2a8ae03eec2d1f4b51d5100080147ed838aa34f
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2024-11-19 11:10:51 +0200

    fix: update addresses

--- packages/auth-server/stores/client.ts
+++ packages/auth-server/stores/client.ts
@@ -27,10 +27,10 @@ export const contractsByChain: Record<SupportedChainId, ChainContracts> = {
     accountPaymaster: "0xAdbfC97D4c04e87e939eC6DbE0FCC9968A573900",
   },
   [zksyncInMemoryNode.id]: {
-    session: "0x8543528a4561E3a5EC7d51Bfd3776457b0E7b7dc",
-    passkey: "0x07734BA326b6AD13BfC0115b0903EB14268F1617",
-    accountFactory: "0xaAF5f437fB0524492886fbA64D703df15BF619AE",
-    accountPaymaster: "0x2879853afd8d066ca66e224A8E85621aC43d6F59",
+    session: "0xfC1258A30B6A1Fef5a49E5575578cF67Ddbbe77a",
+    passkey: "0x0B7054753b1562821C4407Ac02B1C778E2Bc5bEe",
+    accountFactory: "0xa20df50c726538d956B23E09457718EC9F85C8df",
+    accountPaymaster: "0xA07cC53e21b452CEAb91d336383789f19d57c77d",
   },
 };
 
