77debe6f673b388528ed437fdcda491a469f2fd5
jcsec-security <jc@jcsec.io>
2025-02-07 10:07:39 +0000

    fix: audit L-03

--- src/managers/OwnerManager.sol
+++ src/managers/OwnerManager.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import { SsoStorage } from "../libraries/SsoStorage.sol";
-import { Auth } from "../auth/Auth.sol";
+import { SelfAuth } from "../auth/SelfAuth.sol";
 import { IOwnerManager } from "../interfaces/IOwnerManager.sol";
 
 /**
@@ -13,7 +13,7 @@ import { IOwnerManager } from "../interfaces/IOwnerManager.sol";
  * @dev Owners are stored in an EnumerableSet
  * @author https://getclave.io
  */
-abstract contract OwnerManager is IOwnerManager, Auth {
+abstract contract OwnerManager is IOwnerManager, SelfAuth {
   using EnumerableSet for EnumerableSet.AddressSet;
 
   /// @inheritdoc IOwnerManager
