faa2750e03b1609e46da27349e3a272af6a853b0
jcsec-security <jc@jcsec.io>
2025-02-09 21:32:58 +0000

    fix: audit N-05

--- src/interfaces/ISsoAccount.sol
+++ src/interfaces/ISsoAccount.sol
@@ -26,5 +26,5 @@ interface ISsoAccount is
   IValidatorManager,
   IAccount
 {
-  function initialize(bytes[] calldata initialValidators, address[] calldata k1Owners) external;
+  function initialize(bytes[] calldata initialValidators, address[] calldata initialK1Owners) external;
 }
