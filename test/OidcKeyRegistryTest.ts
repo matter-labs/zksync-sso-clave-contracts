50526c3799dd40925f0cd8e89ef4b5528a8c85b7
matias-gonz <maigonzalez@fi.uba.ar>
2025-02-11 21:02:45 -0300

    Test should set one key

--- test/OidcKeyRegistryTest.ts
+++ test/OidcKeyRegistryTest.ts
@@ -2,18 +2,35 @@ import { Address } from "viem";
 import { ContractFixtures } from "./utils";
 import { Wallet } from "zksync-ethers";
 import { expect } from "chai";
+import { OidcKeyRegistry, OidcKeyRegistry__factory } from "../typechain-types";
 
 describe("OidcKeyRegistry", function () {
   const fixtures = new ContractFixtures();
-  let oidcKeyRegistryAddress: Address;
-  let deployer: Wallet = fixtures.wallet;
+  let oidcKeyRegistry: OidcKeyRegistry;
 
   this.beforeAll(async () => {
-    const oidcKeyRegistry = await fixtures.getOidcKeyRegistryContract();
-    oidcKeyRegistryAddress = await oidcKeyRegistry.getAddress() as Address;
+    const contract = await fixtures.getOidcKeyRegistryContract();
+    let oidcKeyRegistryAddress = await contract.getAddress() as Address;
+    let deployer: Wallet = fixtures.wallet;
+    oidcKeyRegistry = OidcKeyRegistry__factory.connect(oidcKeyRegistryAddress, deployer);
   });
 
-  it("should deploy OidcKeyRegistry", async () => {
-    expect(oidcKeyRegistryAddress).to.be.properAddress;
+  it("should set one key", async () => {
+    const oidcKeyRegistry = await fixtures.getOidcKeyRegistryContract();
+  
+    const issuer = "https://example.com";
+    const issHash = await oidcKeyRegistry.hashIssuer(issuer);
+    const key = {
+      kid: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+      n: "0xabcdef",
+      e: "0x010001",
+    };
+  
+    await oidcKeyRegistry.setKey(issHash, key);
+  
+    const storedKey = await oidcKeyRegistry.getKey(issHash, key.kid);
+    expect(storedKey.kid).to.equal(key.kid);
+    expect(storedKey.n).to.equal(key.n);
+    expect(storedKey.e).to.equal(key.e);
   });
 });
