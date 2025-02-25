fc754d97392535d90e08840fc74202266b388b9b
matias-gonz <maigonzalez@fi.uba.ar>
2025-02-25 13:51:15 -0300

    refactor: remove casts
    remove casts

--- src/OidcKeyRegistryNoMerkleComputation.sol
+++ src/OidcKeyRegistryNoMerkleComputation.sol
@@ -39,13 +39,14 @@ contract OidcKeyRegistryNoMerkleComputation is Initializable, OwnableUpgradeable
   }
 
   function addKeys(Key[] memory newKeys, bytes32 root) public onlyOwner {
+    uint8 nextIndex = keyIndex;
     for (uint8 i = 0; i < newKeys.length; i++) {
-      uint8 nextIndex = (keyIndex + 1 + i) % MAX_KEYS; // Circular buffer
+      nextIndex = (keyIndex + 1 + i) % MAX_KEYS; // Circular buffer
       OIDCKeys[nextIndex] = newKeys[i];
     }
 
     merkleRoot = root;
-    keyIndex = uint8((uint256(keyIndex) + newKeys.length) % uint256(MAX_KEYS));
+    keyIndex = nextIndex;
   }
 
   function getKey(bytes32 issHash, bytes32 kid) public view returns (Key memory) {
