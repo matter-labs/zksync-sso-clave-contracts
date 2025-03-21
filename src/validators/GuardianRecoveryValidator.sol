ec565fc1c5c7f1b17dbeabfb872082c10d005fdf
Lukasz Romanowski <5160687+MiniRoman@users.noreply.github.com>
2025-03-21 07:38:03 +0100

    fix(N-12): add security contact

--- src/validators/GuardianRecoveryValidator.sol
+++ src/validators/GuardianRecoveryValidator.sol
@@ -12,6 +12,10 @@ import { IModule } from "../interfaces/IModule.sol";
 import { TimestampAsserterLocator } from "../helpers/TimestampAsserterLocator.sol";
 import { BatchCaller, Call } from "../batch/BatchCaller.sol";
 
+/// @title GuardianRecoveryValidator
+/// @author Matter Labs
+/// @custom:security-contact security@matterlabs.dev
+/// @dev This contract allows account recovery using trusted guardians.
 contract GuardianRecoveryValidator is Initializable, IGuardianRecoveryValidator {
   using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
 
